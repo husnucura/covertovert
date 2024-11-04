e1512b45dfbf261379902673a8c9e91b4b81a673
Budique <turkmenberke11@gmail.com>
2024-11-04 14:54:09 +0300

    Removed prints

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -4,9 +4,7 @@ def packet_callback(packet):
     if packet.haslayer(ICMP):
         ip_layer = packet[IP]
         if ip_layer.ttl == 1:
-            print("Received ICMP packet with TTL=1:")
-            print(packet.show())
+            packet.show()
 
 
-print("Starting ICMP receiver...")
 sniff(filter="icmp", prn=packet_callback)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -3,4 +3,3 @@ from scapy.all import IP, ICMP, send
 packet = IP(dst="receiver") / ICMP()
 packet.ttl = 1 
 send(packet)
-print(f"ICMP request sent to {packet.dst} with TTL={packet.ttl}")
