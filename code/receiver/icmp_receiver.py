89dc00b7f0902d6f7ab9d592c138bbb896765cbf
Husnu Cura <husnu.cura@gmail.com>
2024-11-04 14:25:11 +0300

    implemented receiver and sender

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,12 @@
-import scapy
+from scapy.all import ICMP, IP, sniff
 
-# Implement your ICMP receiver here
+def packet_callback(packet):
+    if packet.haslayer(ICMP):
+        ip_layer = packet[IP]
+        if ip_layer.ttl == 1:
+            print("Received ICMP packet with TTL=1:")
+            print(packet.show())
+
+
+print("Starting ICMP receiver...")
+sniff(filter="icmp", prn=packet_callback)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,6 @@
-import scapy
+from scapy.all import IP, ICMP, send
 
-# Implement your ICMP sender here
+packet = IP(dst="receiver") / ICMP()
+packet.ttl = 1 
+send(packet)
+print(f"ICMP request sent to {packet.dst} with TTL={packet.ttl}")
